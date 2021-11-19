56571669b8eb8d9f3feb9282161b39c8b34ee55c
Gehad Eldibany <86535343+gehadibany@users.noreply.github.com>
2021-11-19 06:58:25 +0200

    Update German_Dictionary.py

--- German_Dictionary.py
+++ German_Dictionary.py
@@ -4,26 +4,27 @@
 # In[1]:
 
 
-import ast
+import ast 
 file_reader = open( 'german_dictionary.txt', 'r' )
 german_dic  = ast.literal_eval( file_reader.read( ) )
-dcontntinue = 'y'
-while dcontntinue == 'y':
-    x = input('Enter the word in German:')
+
+x =''
+while x != 'x':
+    x = input('Enter a word in German or press x to quit:')
     check =x in german_dic
-    if check == True:
-        print ('The meaning is', german_dic[x])
-        
+    if x == 'x':
+        break
+    elif check == True:
+        print ('The meaning is:', german_dic[x])    
     else:
         german_dic[x] = input("Not found. Enter the word in English to be added:")
         print ('Sucessfully added!')
     
-    dcontntinue = input('Do you want to continue? y/n')
-
 file_writer = open( 'german_dictionary.txt', 'w+' )
 file_writer.write(str(german_dic))
 file_writer.close()
-print ('Thank you! Come again later!')
+print ('Danke! Tschüss!')
+
 
 
 # In[ ]:
